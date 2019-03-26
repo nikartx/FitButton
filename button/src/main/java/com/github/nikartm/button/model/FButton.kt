29e1b4841ff09d4d37f209b8c665066a87d864be
Ivan V <jvissun@gmail.com>
2019-03-26 23:57:53 +0300

    Added fit btn models

---
+++ button/src/main/java/com/github/nikartm/button/model/FButton.kt
@@ -0,0 +1,47 @@
+package com.github.nikartm.button.model
+
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
+import android.view.View
+
+/**
+ * @author Ivan V on 25.03.2019.
+ * @version 1.0
+ */
+data class FButton(
+        var image: Drawable? = null,
+        var imgColor: Int = 0,
+        var imgBackColor: Int = 0,
+        var imgWidth: Float = 0f,
+        var imgHeight: Float = 0f,
+        var imgPaddingTop: Float = 0f,
+        var imgPaddingBottom: Float = 0f,
+        var imgPaddingStart: Float = 0f,
+        var imgPaddingEnd: Float = 0f,
+        var imgGravity: Float = 0f,
+
+        var divColor: Int = 0,
+        var divWidth: Float = 0f,
+        var divHeight: Float = 0f,
+        var divMarginTop: Float = 0f,
+        var divMarginBottom: Float = 0f,
+
+        var text: String? = null,
+        var textFont: Typeface? = null,
+        var textStyle: Int = 0,
+        var textSize: Int = 0,
+        var textColor: Int = 0,
+        var textAllCaps: Boolean = false,
+
+        var btnColor: Int = 0,
+        var cornerRadius: Float = 0f,
+        var showRipple: Boolean = false,
+        var btnShape: Shape = Shape.RECTANGLE,
+
+        var borderColor: Int = 0,
+        var borderWidth: Float = 0f,
+
+        var imgVisibility: Int = View.VISIBLE,
+        var divVisibility: Int = View.GONE,
+        var textVisibility: Int = View.VISIBLE
+)
---
+++ button/src/main/java/com/github/nikartm/button/model/Shape.kt
@@ -0,0 +1,10 @@
+package com.github.nikartm.button.model
+
+/**
+ * Kind of shapes [FButton]
+ * @author Ivan V on 25.03.2019.
+ * @version 1.0
+ */
+enum class Shape {
+    RECTANGLE, SQUARE, CIRCLE
+}

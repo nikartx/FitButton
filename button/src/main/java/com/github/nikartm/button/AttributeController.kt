515b1aec67b703b634ac0422a9c777d74d8c03e1
Ivan V <jvissun@gmail.com>
2019-03-27 11:58:57 +0300

    Icon position attr was added

--- button/src/main/java/com/github/nikartm/button/AttributeController.kt
+++ button/src/main/java/com/github/nikartm/button/AttributeController.kt
@@ -6,13 +6,14 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import com.github.nikartm.button.model.FButton
+import com.github.nikartm.button.model.IconPosition
 import com.github.nikartm.button.model.Shape
 
 /**
  * @author Ivan V on 25.03.2019.
  * @version 1.0
  */
-class AttributeController(private val view: View, private val attrs: AttributeSet) {
+internal class AttributeController(private val view: View, private val attrs: AttributeSet) {
 
     /**
      * @return initialized [FButton]
@@ -26,22 +27,23 @@ class AttributeController(private val view: View, private val attrs: AttributeSe
     private fun initAttr() {
         val typedArray = view.context.obtainStyledAttributes(attrs, R.styleable.FitButton)
         // Init button image
-        val image = typedArray.getDrawable(R.styleable.FitButton_fb_image)
-        val imgColor = typedArray.getColor(R.styleable.FitButton_fb_imageColor, Color.TRANSPARENT)
-        val imgBackColor = typedArray.getColor(R.styleable.FitButton_fb_imageBackGroundColor, 0)
-        val imgWidth = typedArray.getDimension(
-                R.styleable.FitButton_fb_imageWidth, image?.intrinsicWidth?.toFloat() ?: 0f
+        val icon = typedArray.getDrawable(R.styleable.FitButton_fb_icon)
+        val iconColor = typedArray.getColor(R.styleable.FitButton_fb_iconColor, Color.TRANSPARENT)
+        val iconBackColor = typedArray.getColor(R.styleable.FitButton_fb_iconBackGroundColor, 0)
+        val iconWidth = typedArray.getDimension(
+                R.styleable.FitButton_fb_iconWidth, icon?.intrinsicWidth?.toFloat() ?: 0f
         )
-        val imgHeight = typedArray.getDimension(
-                R.styleable.FitButton_fb_imageHeight, image?.intrinsicHeight?.toFloat() ?: 0f
+        val iconHeight = typedArray.getDimension(
+                R.styleable.FitButton_fb_iconHeight, icon?.intrinsicHeight?.toFloat() ?: 0f
         )
-        val imgPadding = typedArray.getDimension(R.styleable.FitButton_fb_imagePadding, 0f)
-        val imgPaddingTop = typedArray.getDimension(R.styleable.FitButton_fb_imagePaddingTop, 0f)
-        val imgPaddingBottom = typedArray.getDimension(R.styleable.FitButton_fb_imagePaddingBottom, 0f)
-        val imgPaddingStart = typedArray.getDimension(R.styleable.FitButton_fb_imagePaddingStart, 0f)
-        val imgPaddingEnd = typedArray.getDimension(R.styleable.FitButton_fb_imagePaddingEnd, 0f)
-        val imgGravity = typedArray.getInt(R.styleable.FitButton_fb_imageGravity, Gravity.CENTER)
-        val imgVisibility = typedArray.getInt(R.styleable.FitButton_fb_imageVisibility, View.VISIBLE)
+        val iconPadding = typedArray.getDimension(R.styleable.FitButton_fb_iconPadding, 0f)
+        val iconPaddingTop = typedArray.getDimension(R.styleable.FitButton_fb_iconPaddingTop, 0f)
+        val iconPaddingBottom = typedArray.getDimension(R.styleable.FitButton_fb_iconPaddingBottom, 0f)
+        val iconPaddingStart = typedArray.getDimension(R.styleable.FitButton_fb_iconPaddingStart, 0f)
+        val iconPaddingEnd = typedArray.getDimension(R.styleable.FitButton_fb_iconPaddingEnd, 0f)
+        val iconGravity = typedArray.getInt(R.styleable.FitButton_fb_iconGravity, Gravity.CENTER)
+        val iconPosition = typedArray.getInt(R.styleable.FitButton_fb_iconPosition, IconPosition.LEFT.position)
+        val iconVisibility = typedArray.getInt(R.styleable.FitButton_fb_iconVisibility, View.VISIBLE)
 
         // Init divider
         val divColor = typedArray.getColor(R.styleable.FitButton_fb_divColor, Color.DKGRAY)
@@ -65,24 +67,25 @@ class AttributeController(private val view: View, private val attrs: AttributeSe
         val btnColor = typedArray.getColor(R.styleable.FitButton_fb_backgroundColor, 0)
         val cornerRadius = typedArray.getDimension(R.styleable.FitButton_fb_cornerRadius, 0f)
         val showRipple = typedArray.getBoolean(R.styleable.FitButton_fb_showRipple, true)
-        val shape = typedArray.getInt(R.styleable.FitButton_fb_shape, 0)
+        val shape = typedArray.getInt(R.styleable.FitButton_fb_shape, Shape.RECTANGLE.position)
 
         // Init border
         val borderColor = typedArray.getColor(R.styleable.FitButton_fb_borderColor, Color.TRANSPARENT)
         val borderWidth = typedArray.getDimension(R.styleable.FitButton_fb_borderWidth, 0f)
 
-        button = FButton(image,
-                imgColor,
-                imgBackColor,
-                imgWidth,
-                imgHeight,
-                imgPadding,
-                imgPaddingTop,
-                imgPaddingBottom,
-                imgPaddingStart,
-                imgPaddingEnd,
-                imgGravity,
-                imgVisibility,
+        button = FButton(icon,
+                iconColor,
+                iconBackColor,
+                iconWidth,
+                iconHeight,
+                iconPadding,
+                iconPaddingTop,
+                iconPaddingBottom,
+                iconPaddingStart,
+                iconPaddingEnd,
+                iconGravity,
+                IconPosition.values().first { ic -> ic.position == iconPosition },
+                iconVisibility,
                 divColor,
                 divWidth,
                 divHeight,
@@ -99,7 +102,7 @@ class AttributeController(private val view: View, private val attrs: AttributeSe
                 btnColor,
                 cornerRadius,
                 showRipple,
-                Shape.values()[shape],
+                Shape.values().first { s -> s.position == shape },
                 borderColor,
                 borderWidth)
 

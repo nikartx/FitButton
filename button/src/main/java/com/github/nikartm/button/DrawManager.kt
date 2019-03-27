f1ad1787b0017ed3ab9ca83457890e2011c7f744
Ivan V <jvissun@gmail.com>
2019-03-27 15:08:01 +0300

    Fixed draw manager

--- button/src/main/java/com/github/nikartm/button/DrawManager.kt
+++ button/src/main/java/com/github/nikartm/button/DrawManager.kt
@@ -1,18 +1,25 @@
 package com.github.nikartm.button
 
-import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
+import com.github.nikartm.button.drawer.ContainerDrawer
+import com.github.nikartm.button.drawer.DividerDrawer
+import com.github.nikartm.button.drawer.IconDrawer
+import com.github.nikartm.button.drawer.TextDrawer
 import com.github.nikartm.button.model.FButton
 
 /**
  * @author Ivan V on 25.03.2019.
  * @version 1.0
  */
-internal class DrawManager constructor(private val view: View, private val attrs : AttributeSet) {
+internal class DrawManager constructor(view: View, attrs : AttributeSet?) {
 
     private val controller = AttributeController(view, attrs)
-    private val drawer = ButtonDrawer(view, controller.button)
+
+    private val container = ContainerDrawer(view, controller.button)
+    private val icon = IconDrawer(view, controller.button)
+    private val divider = DividerDrawer(view, controller.button)
+    private val text = TextDrawer(view, controller.button)
 
     companion object Create {
         fun init(view: View, attrs : AttributeSet): DrawManager {
@@ -20,10 +27,20 @@ internal class DrawManager constructor(private val view: View, private val attrs
         }
     }
 
-    fun drawButton(canvas: Canvas) {
-        drawer.draw(canvas)
+    /**
+     * Draw customized [FitButton] on [View]
+     */
+    fun drawButton() : DrawManager {
+        container.draw()
+        icon.draw()
+        divider.draw()
+        text.draw()
+        return this
     }
 
+    /**
+     * @return [FButton] with attrs or default values
+     */
     fun getButton() : FButton {
         return controller.button
     }

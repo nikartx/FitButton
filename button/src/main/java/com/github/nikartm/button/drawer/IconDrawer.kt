56fabe11773fedb49221d347b764e0f9c89a0d9d
Ivan V <jvissun@gmail.com>
2019-04-03 03:17:22 +0300

    Fixed icon and text position and orientation

--- button/src/main/java/com/github/nikartm/button/drawer/IconDrawer.kt
+++ button/src/main/java/com/github/nikartm/button/drawer/IconDrawer.kt
@@ -1,16 +1,12 @@
 package com.github.nikartm.button.drawer
 
 import android.text.TextUtils
-import android.view.Gravity
 import android.view.View
-import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.github.nikartm.button.FitButton
 import com.github.nikartm.button.model.FButton
-import com.github.nikartm.button.model.IconPosition
 
 /**
  * @author Ivan V on 27.03.2019.
@@ -20,12 +16,10 @@ internal class IconDrawer(val view: FitButton, val button: FButton)
     : Drawer<FitButton, FButton>(view, button) {
 
     private lateinit var iv: ImageView
-    private lateinit var fl: FrameLayout
 
     override fun draw() {
-        prepareIconLayout()
-        fl.addView(getIcon())
-        view.addView(fl)
+        initIcon()
+        view.addView(iv)
     }
 
     override fun isReady(): Boolean {
@@ -33,55 +27,32 @@ internal class IconDrawer(val view: FitButton, val button: FButton)
                 || button.iconVisibility == View.INVISIBLE)
     }
 
-    private fun prepareIconLayout() {
-        fl = FrameLayout(view.context)
-        fl.layoutParams = when (button.iconPosition) {
-            IconPosition.LEFT, IconPosition.RIGHT -> drawIconLeftRight()
-            IconPosition.TOP, IconPosition.BOTTOM -> drawIconTopBottom()
-            else -> drawIconCenter()
-        }
-        fl.setBackgroundColor(button.iconBackColor)
-    }
-
     // Prepare icon to the drawing
-    private fun getIcon() : ImageView {
+    private fun initIcon() : ImageView {
         iv = ImageView(view.context)
         iv.setImageDrawable(button.icon)
         iv.setColorFilter(button.iconColor)
         iv.visibility = button.iconVisibility
-        val iconParams = FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                button.gravity)
+        val iconParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT, getWeight())
         iconParams.width = button.iconWidth.toInt()
         iconParams.height = button.iconHeight.toInt()
         iconParams.marginStart = button.iconMarginStart.toInt()
         iconParams.topMargin = button.iconMarginTop.toInt()
         iconParams.marginEnd = button.iconMarginEnd.toInt()
         iconParams.bottomMargin = button.iconMarginBottom.toInt()
+        iconParams.gravity = button.gravity
         iv.layoutParams = iconParams
         return iv
     }
 
-    private fun drawIconLeftRight() : FrameLayout.LayoutParams {
-        view.orientation = LinearLayout.HORIZONTAL
-        return FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-    }
-
-    private fun drawIconTopBottom() : FrameLayout.LayoutParams {
-        view.orientation = LinearLayout.VERTICAL
-        return FrameLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-    }
-
-    private fun drawIconCenter() : FrameLayout.LayoutParams {
-        view.orientation = LinearLayout.VERTICAL
-        val params = if (TextUtils.isEmpty(button.text)) {
-            FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-        } else{
-            FrameLayout.LayoutParams(WRAP_CONTENT, MATCH_PARENT)
+    // Get layout weight dependent on the layout orientation and a text state
+    private fun getWeight() : Float {
+        return when(view.orientation) {
+            LinearLayout.VERTICAL -> {
+                if (TextUtils.isEmpty(button.text)) 0f else 1f
+            }
+            else -> 0f
         }
-        params.gravity = button.gravity
-        return params
     }
 
 }
--- button/src/main/java/com/github/nikartm/button/drawer/TextDrawer.kt
+++ button/src/main/java/com/github/nikartm/button/drawer/TextDrawer.kt
@@ -2,8 +2,10 @@ package com.github.nikartm.button.drawer
 
 import android.graphics.Typeface
 import android.text.TextUtils
-import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.github.nikartm.button.FitButton
 import com.github.nikartm.button.getDensity
@@ -19,6 +21,15 @@ internal class TextDrawer(val view: FitButton, val button: FButton)
     private lateinit var tv: TextView
 
     override fun draw() {
+        initText()
+        view.addView(tv)
+    }
+
+    override fun isReady(): Boolean {
+        return !TextUtils.isEmpty(button.text) && button.textVisibility != View.GONE
+    }
+
+    private fun initText() {
         tv = TextView(view.context)
         tv.text = button.text
         tv.typeface = Typeface.create(button.textFont, button.textStyle)
@@ -26,17 +37,15 @@ internal class TextDrawer(val view: FitButton, val button: FButton)
         tv.setTextColor(button.textColor)
         tv.isAllCaps = button.textAllCaps
         tv.visibility = button.textVisibility
-        tv.gravity = Gravity.CENTER
-        if (button.icon == null || button.iconVisibility == View.GONE) {
-            view.gravity = Gravity.CENTER
-        }
+        tv.gravity = button.textGravity
         tv.setPadding(button.textPaddingStart.toInt(), button.textPaddingTop.toInt(),
                 button.textPaddingEnd.toInt(), button.textPaddingBottom.toInt())
-        view.addView(tv)
-    }
+        if (view.orientation == LinearLayout.HORIZONTAL) {
+            tv.layoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, MATCH_PARENT, 1f)
+        } else {
+            tv.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT, 1f)
 
-    override fun isReady(): Boolean {
-        return !TextUtils.isEmpty(button.text) && button.textVisibility != View.GONE
+        }
     }
 
 }

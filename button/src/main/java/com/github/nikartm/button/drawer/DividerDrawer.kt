b0f4aff65ee27f1c79c6e390062b49c5c89af523
Ivan V <jvissun@gmail.com>
2019-04-04 02:14:37 +0300

    Fixed the divider measure

--- button/src/main/java/com/github/nikartm/button/drawer/DividerDrawer.kt
+++ button/src/main/java/com/github/nikartm/button/drawer/DividerDrawer.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.LinearLayout
 import com.github.nikartm.button.FitButton
 import com.github.nikartm.button.model.FButton
+import com.github.nikartm.button.model.Shape
 
 /**
  * @author Ivan V on 27.03.2019.
@@ -31,21 +32,36 @@ internal class DividerDrawer(val view: FitButton, val button: FButton)
         div.setBackgroundColor(button.divColor)
         div.visibility = button.divVisibility
         divParams = ViewGroup.MarginLayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-        if (button.divHeight == 0f && view.orientation == LinearLayout.HORIZONTAL) {
-            divParams.height = (view.layoutParams.height - (button.borderWidth * 2f).toInt())
-        } else {
-            divParams.height = button.divHeight.toInt()
-        }
-        if (button.divWidth == 0f && view.orientation == LinearLayout.VERTICAL) {
-            divParams.width = (view.layoutParams.width - (button.borderWidth * 2f).toInt())
-        } else {
-            divParams.width = button.divWidth.toInt()
-        }
         divParams.marginStart = button.divMarginStart.toInt()
         divParams.topMargin = button.divMarginTop.toInt()
         divParams.marginEnd = button.divMarginEnd.toInt()
         divParams.bottomMargin = button.divMarginBottom.toInt()
+        setMeasure()
         div.layoutParams = divParams
+    }
+
+    // Set the divider measure if a shape of the button was changed
+    private fun setMeasure() {
+        val minMeasure = Math.min(view.measuredWidthAndState, view.measuredHeightAndState)
+        val borderMeasure = (button.borderWidth * 2f).toInt()
+        if (button.divHeight == 0f && view.orientation == LinearLayout.HORIZONTAL) {
+            val wMeas = when(button.btnShape) {
+                Shape.SQUARE, Shape.CIRCLE -> minMeasure
+                else -> view.measuredHeightAndState
+            }
+            divParams.height = (wMeas - borderMeasure)
+        } else {
+            divParams.height = button.divHeight.toInt()
+        }
+        if (button.divWidth == 0f && view.orientation == LinearLayout.VERTICAL) {
+            val hMeas = when(button.btnShape) {
+                Shape.SQUARE, Shape.CIRCLE -> minMeasure
+                else -> view.measuredWidthAndState
+            }
+            divParams.width = (hMeas - borderMeasure)
+        } else {
+            divParams.width = button.divWidth.toInt()
+        }
     }
 
 }

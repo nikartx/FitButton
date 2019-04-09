da44799699005ed3247cf066930f034b4dcee97e
Ivan V <jvissun@gmail.com>
2019-04-09 14:32:21 +0300

    Added example to programmatically initialization of the button

--- app/src/main/java/com/github/nikartm/MainActivity.kt
+++ app/src/main/java/com/github/nikartm/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.github.nikartm
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-
+import android.widget.Toast
 import com.github.nikartm.button.FitButton
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +15,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupButton()
+    }
 
+    private fun setupButton() {
         fitButton = findViewById(R.id.fbtn)
-        Log.d(TAG, "onCreate: " + fitButton!!.getIconWidth())
+        fitButton!!
+                .setTextFont(R.font.share_tech_regular)
+                .setTextSize(20f)
+                .setIconMarginStart(16f)
+                .setIconMarginEnd(12f)
+                .setTextColor(Color.parseColor("#F5F5F5"))
+                .setIconColor(Color.parseColor("#FFE0B2"))
+                .setDividerColor(Color.parseColor("#BCAAA4"))
+                .setBorderColor(Color.parseColor("#FFF59D"))
+                .setButtonColor(Color.parseColor("#FF7043"))
+                .setBorderWidth(2f)
+                .setRippleEnable(true)
+                .setRippleColor(resources.getColor(R.color.colorAccent))
+                .setOnClickListener {
+                    Toast.makeText(this,
+                            "Click on ${fitButton?.getText()}",
+                            Toast.LENGTH_SHORT).show()
+                }
     }
 
 }

fc04c3f65d9359fc8e1750e9bd66e7fb8def28d4
Ivan V <jvissun@gmail.com>
2019-04-14 21:21:38 +0300

    Fixed examples

--- app/src/main/java/com/github/nikartm/MainActivity.kt
+++ app/src/main/java/com/github/nikartm/MainActivity.kt
@@ -2,9 +2,11 @@ package com.github.nikartm
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.nikartm.button.FitButton
+import com.github.nikartm.button.model.Shape
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +28,7 @@ class MainActivity : AppCompatActivity() {
                 .setIconMarginStart(16f)
                 .setIconMarginEnd(12f)
                 .setTextColor(Color.parseColor("#F5F5F5"))
-                .setIconColor(Color.parseColor("#FFE0B2"))
+                .setIconColor(Color.parseColor("#FFFFFF"))
                 .setDividerColor(Color.parseColor("#BCAAA4"))
                 .setBorderColor(Color.parseColor("#FFF59D"))
                 .setButtonColor(Color.parseColor("#FF7043"))
@@ -34,10 +36,16 @@ class MainActivity : AppCompatActivity() {
                 .setRippleEnable(true)
                 .setRippleColor(resources.getColor(R.color.colorAccent))
                 .setOnClickListener {
+//                    changeButton()
                     Toast.makeText(this,
                             "Click on ${fitButton?.getText()}",
                             Toast.LENGTH_SHORT).show()
                 }
+    }
+
+    private fun changeButton() {
+        fitButton?.setButtonShape(Shape.CIRCLE)
+                ?.setDividerVisibility(View.GONE)
     }
 
 }
